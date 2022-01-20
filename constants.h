37c13b16a6b81e6660c5b5846fae9898348ccc55
MarinaSenoshenko <94277819+MarinaSenoshenko@users.noreply.github.com>
2022-01-20 18:12:38 +0700

    Update constants.h

--- constants.h
+++ constants.h
@@ -1,7 +1,12 @@
-#define UNREAL_FILE_LEN -1
-#define ZERO_LEN 0
-#define EXIT 0
-#define BELOW_ZERO_OFFSET 0
-#define BEGINING_SIZE 0
-#define NEXT_POS 1
-#define PREV_POS 1
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+constexpr int UNREAL_FILE_LEN = -1;
+constexpr int ZERO_LEN = 0;
+constexpr int EXIT = 0;
+constexpr int BELOW_ZERO_OFFSET = 0;
+constexpr int BEGINING_SIZE = 0;
+constexpr int NEXT_POS = 1;
+constexpr int PREV_POS = 1;
+
+#endif
